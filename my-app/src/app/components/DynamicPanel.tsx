1c2b9d5f11fc3e774e537a1c4d4bd8bc91cde71f
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-11 15:39:05 +0100

    chore: details

--- my-app/src/app/components/DynamicPanel.tsx
+++ my-app/src/app/components/DynamicPanel.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import Chart from 'chart.js/auto';
+import React, { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
 
 export const DynamicPanel: React.FC = () => {
   const [isPanelVisible, setPanelVisible] = useState(false);
@@ -11,33 +11,37 @@ export const DynamicPanel: React.FC = () => {
 
   useEffect(() => {
     if (isPanelVisible) {
-      const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+      const ctx = document.getElementById("myChart") as HTMLCanvasElement;
       if (ctx) {
+        Chart.getChart(ctx)?.destroy();
+
         new Chart(ctx, {
-          type: 'bar',
+          type: "bar",
           data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            }],
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [
+              {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(255, 206, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)",
+                  "rgba(75, 192, 192, 1)",
+                  "rgba(153, 102, 255, 1)",
+                  "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+              },
+            ],
           },
         });
       }
@@ -51,13 +55,14 @@ export const DynamicPanel: React.FC = () => {
         className="bg-blue-500 text-white px-4 py-2 rounded-md mb-4 transition duration-300 ease-out"
         onClick={togglePanel}
       >
-        {isPanelVisible ? 'Hide Graph' : 'See Graph'}
+        {isPanelVisible ? "Hide Graph" : "See Graph"}
       </button>
 
       <div
-        className={`bg-gray-200 box-content p-4 rounded-md shadow-md transition-opacity transition-height duration-300 ease-out ${isPanelVisible ? 'opacity-100 h-auto' : 'opacity-0 h-0'}`}
+        className={`bg-gray-100 box-content p-4 rounded-md shadow-md transition-opacity transition-height duration-300 ease-out ${
+          isPanelVisible ? "opacity-100 h-auto" : "opacity-0 h-0"
+        }`}
       >
-
         <canvas id="myChart" width={500} height={250}></canvas>
       </div>
     </div>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,11 +4,12 @@ import {DynamicPanel} from './components/DynamicPanel';
 
 
 const Home: React.FC = () => {
-
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl font-bold mt-12 mb-12 text-center">Dashboard</h1>
-      <DynamicPanel />
+    <div className="min-h-screen">
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-bold mt-12 mb-12 text-center">Dashboard</h1>
+        <DynamicPanel />
+      </div>
     </div>
   );
 };
