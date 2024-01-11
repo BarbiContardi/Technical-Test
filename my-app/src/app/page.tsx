825bfbc846dcb884707481c60eb4fee2da44445a
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-11 14:55:24 +0100

    feat: Animated UI Components

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {DynamicPanel} from './components/DynamicPanel';
+
+
+
+const Home: React.FC = () => {
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold mt-12 mb-12 text-center">Dashboard</h1>
+      <DynamicPanel />
+    </div>
+  );
+};
+
+export default Home;
