54060fb6d22d17fa6aa2cba17544dc467eb0d384
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-12 10:12:33 +0100

    fix: Api ComponentA

--- my-app/src/app/components/ComponentA.tsx
+++ my-app/src/app/components/ComponentA.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import useDataFetching from '../hooks/useDataFetching';
 
 const ComponentA: React.FC = () => {
-  const { data, loading, error } = useDataFetching("http://numbersapi.com/random/year?json");
+  const { data, loading, error } = useDataFetching("https://api.chucknorris.io/jokes/random");
 
   return (
     <div className='flex flex-col text-center border-4 border-white w-60'>
-      <h1 className='text-lg font-bold mb-2'>RANDOM YEAR DATA</h1>
+      <h1 className='text-lg font-bold mb-2'>CHUCK NORRIS FACTS</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error..</p>}
-      <p>{data?.text}</p>
+      <p>{data?.value}</p>
     </div>
   );
 };
