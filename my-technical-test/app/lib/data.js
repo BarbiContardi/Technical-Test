e7feb9504f06842d971f9c374e4ad549898f925c
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-17 01:31:00 +0100

    fix: tsconfig

--- my-technical-test/app/lib/data.js
+++ my-technical-test/app/lib/data.js
@@ -1,14 +1,14 @@
 import { Product, User } from "./models";
 import { connectToDB } from "./utils";
 
-export const fetchUsers = async (q: string, page: number) => {
+export const fetchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
 
   const ITEM_PER_PAGE = 2;
 
   try {
-    await connectToDB();
-    const count = await User.find({ username: { $regex: regex } }).countDocuments();
+    connectToDB();
+    const count = await User.find({ username: { $regex: regex } }).count();
     const users = await User.find({ username: { $regex: regex } })
       .limit(ITEM_PER_PAGE)
       .skip(ITEM_PER_PAGE * (page - 1));
@@ -19,9 +19,10 @@ export const fetchUsers = async (q: string, page: number) => {
   }
 };
 
-export const fetchUser = async (id: string) => {
+export const fetchUser = async (id) => {
+  console.log(id);
   try {
-    await connectToDB();
+    connectToDB();
     const user = await User.findById(id);
     return user;
   } catch (err) {
@@ -30,14 +31,15 @@ export const fetchUser = async (id: string) => {
   }
 };
 
-export const fetchProducts = async (q: string, page: number) => {
+export const fetchProducts = async (q, page) => {
+  console.log(q);
   const regex = new RegExp(q, "i");
 
   const ITEM_PER_PAGE = 2;
 
   try {
-    await connectToDB();
-    const count = await Product.find({ title: { $regex: regex } }).countDocuments();
+    connectToDB();
+    const count = await Product.find({ title: { $regex: regex } }).count();
     const products = await Product.find({ title: { $regex: regex } })
       .limit(ITEM_PER_PAGE)
       .skip(ITEM_PER_PAGE * (page - 1));
@@ -48,9 +50,9 @@ export const fetchProducts = async (q: string, page: number) => {
   }
 };
 
-export const fetchProduct = async (id: string) => {
+export const fetchProduct = async (id) => {
   try {
-    await connectToDB();
+    connectToDB();
     const product = await Product.findById(id);
     return product;
   } catch (err) {
@@ -58,7 +60,6 @@ export const fetchProduct = async (id: string) => {
     throw new Error("Failed to fetch product!");
   }
 };
-
 
 // DUMMY DATA
 
