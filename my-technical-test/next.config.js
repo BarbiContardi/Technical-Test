7a0558478864fdd451850b42112f1efe5a01247b
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-22 13:19:45 +0100

    fix: .env

--- my-technical-test/next.config.js
+++ my-technical-test/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    MONGO: "mongodb+srv://vercel-admin-user:foOzMOCq1MWHgcO8@cluster0.mcyiuhz.mongodb.net/?retryWrites=true&w=majority",
+  },
   images: {
     remotePatterns: [
       {
