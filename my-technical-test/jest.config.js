8529b8f9bd5fafb9b40c4ca2e26999fdd3e31ba0
Barbara Contardi <barbi.contardi@gmail.com>
2024-01-30 10:21:46 +0100

    fix: homepage

--- my-technical-test/jest.config.js
+++
@@ -1,9 +0,0 @@
-import nextJest from 'next/jest'
-
-const createJestConfig = nextJest({ dir: './' })
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
-
-export default createJestConfig(customJestConfig)
